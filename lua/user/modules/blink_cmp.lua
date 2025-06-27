13f498ecef7adf6bfc473e2d23c65adf60a59f99
Kadir Vedachalam <kadir.v@gmail.com>
2025-06-27 17:12:26 +0530

    autocomplete - blink.cmp

--- lua/user/modules/blink_cmp.lua
+++ lua/user/modules/blink_cmp.lua
@@ -1,21 +1,18 @@
+-- blink.cmp
+
 return {
   "Saghen/blink.cmp",
-  build = "cargo build --release",  -- 👈 Compile native matcher
+  version = "1.*",  -- ✅ Use tagged release with prebuilt binaries
   event = "InsertEnter",
-  dependencies = {
-    "Saghen/blink.nvim",
-  },
+  dependencies = { "Saghen/blink.nvim" },
   config = function()
     require("blink.cmp").setup({
-      popup = {
-        border = "rounded",
-        winblend = 10,
-      },
+      popup = { border = "rounded", winblend = 10 },
       keymaps = {
         confirm = "<CR>",
-        cancel = "<Esc>",
-        next = "<Tab>",
-        prev = "<S-Tab>",
+        cancel  = "<Esc>",
+        next    = "<Tab>",
+        prev    = "<S-Tab>",
       },
     })
   end,
